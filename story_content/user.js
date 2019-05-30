1ca29c6b43314c06214c6575c1e703c6f00518cb
Pattinm <48574402+Pattinm@users.noreply.github.com>
2019-05-30 12:49:56 +0930

    live pilot

--- story_content/user.js
+++ story_content/user.js
@@ -1,1 +1,43 @@
-// User defined code goes here
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5splSUwfQEn":
+        Script1();
+        break;
+      case "6jr4gWr4ktV":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  function getQueryVariable(variable) {
+        //window.alert("getQuery function started");
+          var query = window.location.search.substring(1);
+          var vars = query.split("&");
+          for (var i=0;i<vars.length;i++) {
+                var pair = vars[i].split("=");
+                if(pair[0] == variable){return pair[1];}
+              }
+         return(false);
+   };
+var player = GetPlayer();
+var pID = getQueryVariable("ID");
+//window.alert("pID is " + pID);
+player.SetVar("FIFDparticipantIDTwo",pID);
+alert("Welcome back, " + player.GetVar("FIFDparticipantIDTwo") + ".");
+
+}
+
+function Script2()
+{
+  var player=GetPlayer();
+var returnedParticipantID=player.GetVar("FIFDparticipantIDTwo");
+var customURL="http://adelaide.qualtrics.com/jfe/form/SV_3UD4OxJjOjuvsMJ?ID=" + returnedParticipantID;
+//window.alert("Custom URL is " + customURL);
+window.open(customURL);
+
+}
+
